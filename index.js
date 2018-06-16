2582ea9f15eee994bab2d1cf364085fac57dcbda
Sarah Schaller <sarahcschaller@gmail.com>
2018-06-16 18:23:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function findMatching(drivers, name) {
 
 
 function fuzzyMath(drivers, letters) {
-  const chars = name.length;
+  let chars = name.length;
   return drivers.filter(function(driverNames) {
     return driverNames.slice(0, chars) === letters;
   });
